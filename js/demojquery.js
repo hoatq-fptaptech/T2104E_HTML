a3b58ffe4bfb535b3e4685ad1d4e78337e968a60
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-07-03 19:55:45 +0700

    demo js buoi 2 - demo event - DOM element

--- js/demojquery.js
+++ js/demojquery.js
@@ -1,15 +1,16 @@
 function loadMore() {
     var p = [];// ti nua se lay tu
     $.ajax({ //
-        url: "data/products.json",
+        url: "https://foodgroup.herokuapp.com/api/today-special", // giống như dùng file todaySpecial.json -- goi la Web API
         method:"GET",
         success:function (rs){ // callback
-            p = rs;
+            p = rs.data; // chúng ta cần 1 array
             var row = $("#row");
             for (var i = 0; i < p.length; i++) {
                 var ct = "<div class=\"item\">\n" +
                     "            <img src=\"" + p[i].image + "\"/>\n" +
                     "            <h2>" + p[i].name + "</h2>\n" +
+                    "            <h3>" + p[i].price + "</h3>\n" +
                     "        </div>"; // ct chi la 1 string thoi
                 row.append(ct); // row.html(row.html() +ct)
                 // row.prepend(ct) // row.html(ct+row.html());
